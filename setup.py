ffa5fbf3b0ca93e863e0651cdb9aa40391106f37
陆志鹏 <1>
2023-05-19 13:47:49 +0800

    first commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="gpterminal",
-    version="0.1.8",
+    version="0.1.9",
     author="Your Name",
     author_email="your.email@example.com",
     description="A CLI tool to interact with GPT-3/GPT-4, answer Git questions, and execute commands",
